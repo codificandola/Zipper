8e6f540ab70e0bc54ddb50e00668319e84ae0cfa
Codificandola <andrescifuentes09@gmail.com>
2015-08-21 17:56:45 -0300

    V. 1.0

--- zipper.py
+++ zipper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Copyright (C) 2015  Andres Cifuentes
+#    Copyright (C) 2015  Andres Cifuentes
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Zipper V1.0 
+# Zipper V1.0
 
 import os, tarfile, threading, timeit
 
